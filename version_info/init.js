a980e1c7cf50a65356637cf644288b7564b64deb
Greg <supahgreg@users.noreply.github.com>
2021-02-17 06:37:59 -0600

    Fix for required CSRF token

--- version_info/init.js
+++ version_info/init.js
@@ -1,16 +1,17 @@
 require(['dijit/Dialog', 'dojo/ready'], (Dialog, ready) => {
   ready(() => {
     const version_info_endpoint = 'backend.php?op=pluginhandler&plugin=version_info&method=show_version';
-    const dialog = new Dialog({
+        const dialog = new Dialog({
       id: 'showVersionDlg',
       title: __('Version'),
-      href: version_info_endpoint,
     });
-    
-    App.hotkey_actions['show_version'] = () => {
-      dialog.set('href', version_info_endpoint); // force a new request
-      dialog.show();
-    }
+
+    dojo.connect(dialog, 'onShow', () => {
+      xhrPost('backend.php', {op: 'pluginhandler', plugin: 'version_info', method: 'show_version'}, (transport) => {
+        dialog.attr('content', transport.responseText);
+          });
+        });
+
+    App.hotkey_actions['show_version'] = () => { dialog.show(); }
   });
 });
-
