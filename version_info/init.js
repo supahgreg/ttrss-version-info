ec433457e810fac3f545ce8d70f43d56aa711fec
Greg <supahgreg@users.noreply.github.com>
2021-02-17 06:40:27 -0600

    fix whitespace

--- version_info/init.js
+++ version_info/init.js
@@ -1,7 +1,7 @@
 require(['dijit/Dialog', 'dojo/ready'], (Dialog, ready) => {
   ready(() => {
     const version_info_endpoint = 'backend.php?op=pluginhandler&plugin=version_info&method=show_version';
-        const dialog = new Dialog({
+    const dialog = new Dialog({
       id: 'showVersionDlg',
       title: __('Version'),
     });
@@ -9,8 +9,8 @@ require(['dijit/Dialog', 'dojo/ready'], (Dialog, ready) => {
     dojo.connect(dialog, 'onShow', () => {
       xhrPost('backend.php', {op: 'pluginhandler', plugin: 'version_info', method: 'show_version'}, (transport) => {
         dialog.attr('content', transport.responseText);
-          });
-        });
+      });
+    });
 
     App.hotkey_actions['show_version'] = () => { dialog.show(); }
   });
