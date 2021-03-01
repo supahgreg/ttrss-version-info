aabff255dde7ee3627b2c337c23a3520387a0203
supahgreg <supahgreg@users.noreply.github.com>
2021-03-01 06:38:05 -0600

    Oops-- bump to 0.5

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ class Version_Info extends Plugin {
 
   function about() {
     return [
-      0.4, // version
+      0.5, // version
       'Show the tt-rss version using Shift+V.', // description
       'wn', // author
       false, // is system
